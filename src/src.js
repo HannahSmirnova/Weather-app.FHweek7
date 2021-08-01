f06e7371722123f5697a2f849856ea2a167733f4
HannahSmirnova <alexamy1986@gmail.com>
2021-08-01 11:56:36 -0600

    fixed the time to current one

--- src/src.js
+++ src/src.js
@@ -1,25 +1,27 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+let now = new Date();
+let date = now.getDate();
+let year = now.getFullYear();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let time = now.getTime();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
 }
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let currentDay = document.querySelector("#date");
+currentDay.innerHTML = `${day} ${hours}:${minutes}`;
 
 function sunDate(timestamp) {
   let date = new Date(timestamp);
@@ -56,9 +58,6 @@ function displayTemperature(response) {
   );
   document.querySelector("#sunset").innerHTML = sunDate(
     response.data.sys.sunset * 1000
-  );
-  document.querySelector("#date").innerHTML = formatDate(
-    response.data.dt * 1000
   );
   iconElement.setAttribute(
     "src",
