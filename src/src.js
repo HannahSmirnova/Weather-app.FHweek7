62c4447a70b0c02f93ee14ce12e603f267bcc665
HannahSmirnova <alexamy1986@gmail.com>
2021-07-31 21:45:50 -0600

    sunrise,sunset

--- src/src.js
+++ src/src.js
@@ -21,6 +21,19 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function sunDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response);
   let iconElement = document.querySelector("#icon");
@@ -38,10 +51,10 @@ function displayTemperature(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
-  document.querySelector("#sunrise").innerHTML = formatDate(
+  document.querySelector("#sunrise").innerHTML = sunDate(
     response.data.sys.sunrise * 1000
   );
-  document.querySelector("#sunset").innerHTML = formatDate(
+  document.querySelector("#sunset").innerHTML = sunDate(
     response.data.sys.sunset * 1000
   );
   document.querySelector("#date").innerHTML = formatDate(
